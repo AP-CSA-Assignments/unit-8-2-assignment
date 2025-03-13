5874f07545c0df8f765f6590ede9da51ab021731
Kyle Vu <ktvu@hbuhsd.edu>
2025-03-12 22:12:28 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,17 +4,33 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Test code here
+    // TODO: Make an array of doubles, and test your sumArray method
+
+    // TODO: Make an array of Strings, and test your averageLength method
+
+    // TODO: Make an array of ints, and test your arrayMax method
+
+    // TODO: Make an array of Strings as well as a target, and test your linearSearch method
   }
 
 
-  public static int sumOfDiag(int[][] matrix)
+  public static double sumArray(double[] arr)
   {
-    return 0;
+    return 0.0;
   }
 
-  public static int[][] productTable(final int numRows, final int numCols)
+  public static double averageLength(String[] arr)
   {
-    return null;
+    return 0.0;
+  }
+
+  public static int arrayMax(int[] arr)
+  {
+    return Integer.MIN_VALUE;
+  }
+
+  public static boolean linearSearch(String[] arr)
+  {
+    return false;
   }
 }
